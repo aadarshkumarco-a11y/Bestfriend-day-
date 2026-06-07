5e06c28ca305845f3559f3f952d40dbbd5ffeef3
aadarshkumarco-a11y <aadarshkumar.co@gmail.com>
2026-06-07 23:31:26 +0530

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = { title: "💖 Best Friends Day" };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  );
+}
